aaa78e7537b655c073d098e04948fed1aca53053
builditdan <daniels111@gmail.com>
2015-11-29 16:17:50 -0500

    Changes to read and validate a CSV file

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 
 class AddressBook
@@ -25,5 +26,18 @@ attr_accessor :entries
     @entries.each_with_index {| entry, index | found_index = index if (entry.name == name)}
     @entries.delete_at(found_index) if !found_index.nil?
   end
+
+  def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+      csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      end
+
+  end
+
+
 
 end
