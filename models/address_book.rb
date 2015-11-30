e8840e6f493d53b560291cdcbc0647b9f2b9973c
builditdan <daniels111@gmail.com>
2015-11-30 09:47:26 -0500

    Added new binary search and tests

--- models/address_book.rb
+++ models/address_book.rb
@@ -40,9 +40,25 @@ attr_accessor :entries
   end
 
   def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif
+        lower = mid + 1
+      end
+    end
+
     return nil
   end
-  
+
 
 
 
